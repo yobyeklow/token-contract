89ab101268c4c15f37d8793c5a09655f716c1259
Khang <dmin@LAPTOP-26KFMMH8.>
2025-04-25 19:36:11 +0700

    Completed simple contract

--- programs/token-contract/src/lib.rs
+++ programs/token-contract/src/lib.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token;
+use anchor_spl::token::{Token, MintTo,Transfer};
 
 declare_id!("9NqDmt5LXu1FF1Xs8nmt6TFN8nShmM411xTH86o6mN1d");
 
@@ -6,11 +8,52 @@ declare_id!("9NqDmt5LXu1FF1Xs8nmt6TFN8nShmM411xTH86o6mN1d");
 pub mod token_contract {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn mint_token(ctx:Context<MintToken>)-> Result<()>{
+        let cpi_account = MintTo{
+            mint: ctx.accounts.mint.to_account_info(),
+            to: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.payer.to_account_info()
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program,cpi_account);
+        token::mint_to(cpi_ctx,10)?;
+        Ok(())
+    }
+    pub fn transfer_token(ctx:Context<TransferToken>)->Result<()>{
+        let transfer_instruction = Transfer{
+            from: ctx.accounts.from.to_account_info(),
+            to:ctx.accounts.to.to_account_info(),
+            authority:ctx.accounts.signer.to_account_info()
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program,transfer_instruction);
+        anchor_spl::token::transfer(cpi_ctx,5)?;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct MintToken<'info> {
+     /// CHECK: This is the token that we want to mint
+     #[account(mut)]
+     pub mint: UncheckedAccount<'info>,
+     pub token_program: Program<'info, Token>,
+     /// CHECK: This is the token account that we want to mint tokens to
+     #[account(mut)]
+     pub token_account: UncheckedAccount<'info>,
+     /// CHECK: the authority of the mint account
+     #[account(mut)]
+     pub payer: AccountInfo<'info>,
+}
+#[derive(Accounts)]
+pub struct TransferToken<'info>{
+    pub token_program : Program<'info,Token>,
+    /// CHECK: The associated token account that we are transferring the token from
+    #[account(mut)]
+    pub from:UncheckedAccount<'info>,
+    /// CHECK: The associated token account that we are transferring the token to
+    #[account(mut)]
+    pub to:AccountInfo<'info>,
+    #[account(mut)]
+    pub signer:Signer<'info>,
+}
--- tests/token-contract.ts
+++ tests/token-contract.ts
@@ -1,16 +1,47 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { TokenContract } from "../target/types/token_contract";
-
+import { TOKEN_PROGRAM_ID, MINT_SIZE, createAssociatedTokenAccountInstruction,getAssociatedTokenAddress,createInitializeMintInstruction } from "@solana/spl-token";
+import { assert } from "chai";
 describe("token-contract", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
-
+  const mintKey:anchor.web3.Keypair = anchor.web3.Keypair.generate()
   const program = anchor.workspace.tokenContract as Program<TokenContract>;
+  let associatedTokenAccount = undefined
+  it("Mint a token!", async () => {
+    const key = anchor.AnchorProvider.env().wallet.publicKey;
+    const lamports: number = await program.provider.connection.getMinimumBalanceForRentExemption(MINT_SIZE);
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    associatedTokenAccount = await getAssociatedTokenAddress(mintKey.publicKey,key);
+
+    const mintTx = new anchor.web3.Transaction().add(
+      anchor.web3.SystemProgram.createAccount({
+        fromPubkey: key,
+        newAccountPubkey:mintKey.publicKey,
+        space:MINT_SIZE,
+        programId:TOKEN_PROGRAM_ID,
+        lamports,
+      }
+      ),
+      createInitializeMintInstruction(mintKey.publicKey,0,key,key),
+      createAssociatedTokenAccountInstruction(key,associatedTokenAccount,key,mintKey.publicKey)
+    );
+    const res = await anchor.AnchorProvider.env().sendAndConfirm(mintTx,[mintKey])
+
+    console.log("Account: ",res);
+    console.log("Mint Key: ",mintKey.publicKey.toString());
+    console.log("User: ",key.toString())
+
+    const tx = await program.methods.mintToken().accounts({
+      mint: mintKey.publicKey,
+      tokenAccount: associatedTokenAccount,
+      payer: key,
+    }).rpc();
     console.log("Your transaction signature", tx);
+    const minted = (await program.provider.connection.getParsedAccountInfo(associatedTokenAccount)).value.data
+
+    console.log(minted)
+    console.log("Amount NFT: ",minted.parsed.info.tokenAmount)
   });
 });
